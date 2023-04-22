51e6b15e69246c3731741d52730382d05785d679
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-04-22 00:52:26 -0700

    Partially completed the banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="location-name">
+        <h1>HOA VIET MARKET</h1>
+        <h2>Serving Seafoods, Poultry, & Produce</h2>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
