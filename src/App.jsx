16ab93f942959521f0eeb359be1ef5d76631946c
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-04-22 21:32:22 -0700

    Added new Section + Phone Number

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
 
       <div className="location-name">
         <h1>HOA VIET MARKET</h1>
-        <h2>Serving Seafoods, Poultry, & Produce</h2>
+        <h2>Serving Seafoods, Poultry, & Produce&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h2>
+        <img id="phone-icon" src="https://cdn3.iconfinder.com/data/icons/contact-us-set-3/256/5-512.png" alt="yes"></img>
+        <h3>&emsp;(818) 992-7816<br /></h3>
       </div>
 
       {/* <div className='nav-container'>
@@ -41,13 +43,13 @@ function App() {
         </div>
         <div className='ccc'>
           <div className="card-2">
-              <h2>Where We Are</h2>
-              <p>21001 Sherman Way<br />
-                Los Angeles, CA 91303
-              </p>
-              <div>
-                <img id="rest-image"
-                  src="https://maps.googleapis.com/maps/api/staticmap?size=315x150&sensor=false&client=gme-yelp&language=en&scale=1&zoom=15&center=34.202214%2C-118.591014&markers=scale%3A1%7Cicon%3Ahttps%3A%2F%2Fyelp-images.s3.amazonaws.com%2Fassets%2Fmap-markers%2Fannotation_32x43.png%7C34.202214%2C-118.591014&signature=-g2dJgLGpxQIssshGKIocbSNSPk=" alt="My Image" />
+            <h2>Where to Find Us</h2>
+            <p>21001 Sherman Way<br />
+              Los Angeles, CA 91303
+            </p>
+            <div>
+              <img id="rest-image"
+                src="https://maps.googleapis.com/maps/api/staticmap?size=315x150&sensor=false&client=gme-yelp&language=en&scale=1&zoom=15&center=34.202214%2C-118.591014&markers=scale%3A1%7Cicon%3Ahttps%3A%2F%2Fyelp-images.s3.amazonaws.com%2Fassets%2Fmap-markers%2Fannotation_32x43.png%7C34.202214%2C-118.591014&signature=-g2dJgLGpxQIssshGKIocbSNSPk=" alt="My Image" />
             </div>
 
           </div>
@@ -55,10 +57,26 @@ function App() {
             <h2>Our Working Hours</h2>
             <p>Monday - Fridays : &emsp;&emsp;9:00 AM - 9:00 PM<br />Weekends : &emsp;&emsp;&emsp;&emsp;&emsp;9:00 AM - 9:00 PM</p>
           </div>
+          <div className="card-2">
+            <h2>Our Available Amenities</h2>
+            <p>
+              <img id="checkmark" src="https://cdn-icons-png.flaticon.com/512/443/443138.png" alt="yes"></img>
+              &emsp;Parking for customers&emsp;&emsp;&emsp;&emsp;
+              <img id="checkmark" src="https://cdn-icons-png.flaticon.com/512/443/443138.png" alt="yes"></img>
+              &emsp;Accepts credit cards<br />
+              <img id="checkmark" src="https://cdn-icons-png.flaticon.com/512/443/443138.png" alt="yes"></img>
+              &emsp;Wheelchair accessible&emsp;&emsp;&emsp;&emsp;
+              <img id="checkmark" src="https://cdn-icons-png.flaticon.com/512/443/443138.png" alt="yes"></img>
+              &emsp;Air conditioning&emsp;&ensp;&nbsp;</p>
+
+          </div>
         </div>
 
       </div>
 
+      <div>
+        <p>hello</p>
+      </div>
     </>
   )
 }
